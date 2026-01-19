91fba552c8ee3419556a15850ad9d633b40b7fc9
JossSoriano06 <163336284+JossSoriano06@users.noreply.github.com>
2026-01-18 22:51:34 -0500

    base actualizada
    Added new tables and modified existing ones in the database schema. Included color entries and adjusted the detalle_ventas table structure.

--- baseDeDatos/base_inventario.sql
+++ baseDeDatos/base_inventario.sql
@@ -1,5 +1,5 @@
 create database muebles;
-
+use muebles;
 create table clientes (
 	id_clientes int primary key auto_increment,
     nombre_cliente varchar(100) not null,
@@ -20,6 +20,37 @@ create table ventas (
     id_cliente int,
     foreign key (id_cliente) references clientes(id_clientes)
 );
+
+
+
+create table colores (
+colore varchar(100)
+);
+TRUNCATE TABLE colores;
+
+INSERT INTO colores (color) VALUES
+('Iviza'),
+('Rojo'),
+('Azul'),
+('Verde'),
+('Amarillo');
+
+INSERT INTO colores (color) VALUES
+('Iviza'),
+('Chantilly'),
+('Humo'),
+('Wenge'),
+('Cedro'),
+('Lila'),
+('Rosado'),
+('Arena'),
+('Light Sonoma'),
+('Caramelo'),
+('Blanco'),
+('Cedro');
+select * from colores;
+
+
 create table detalle_ventas (
 	id_detalle_venta int primary key auto_increment,
     id_venta int,
@@ -30,3 +61,20 @@ create table detalle_ventas (
     foreign key (id_venta) references ventas (id_venta),
     foreign key (id_producto) references productos (id_producto)
 );
+ALTER TABLE colores CHANGE colore color VARCHAR(100);
+use muebles;
+select * from detalle_ventas;
+ ALTER TABLE detalle_ventas DROP COLUMN colores;
+ ALTER TABLE detalle_ventas
+ADD COLUMN color VARCHAR(100) NOT NULL
+AFTER id_producto;
+
+
+CREATE TABLE boleta_serie (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    serie VARCHAR(4) NOT NULL,
+    correlativo INT NOT NULL
+);
+
+INSERT INTO boleta_serie (serie, correlativo)
+VALUES ('B001', 0);
