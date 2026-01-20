59b76004442f410883317571693928ab0a91c17c
Jose Soriano <sorianomarcasjoseluis@gmail.com>
2026-01-20 04:27:22 -0500

    frontend 2 corregido

--- cliente/src/Producto.js
+++ cliente/src/Producto.js
@@ -192,6 +192,7 @@ function Producto() {
                         </tbody>
                     </table>
                     {productos.length === 0 && !loading && (
+                        //para ver si no hay productos
                         <p className="text-center py-4 text-gray-500">No hay productos registrados.</p>
                     )}
                 </div>
