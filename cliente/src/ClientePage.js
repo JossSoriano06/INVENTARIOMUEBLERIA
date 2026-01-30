52ee78d0fd6bb526ad487cac514f2515bfb5da07
Jose Soriano <sorianomarcasjoseluis@gmail.com>
2026-01-29 23:26:42 -0500

    nueve opcion de colores

--- cliente/src/ClientePage.js
+++ cliente/src/ClientePage.js
@@ -287,6 +287,7 @@ function ClientePage() {
                         <option value="Light Sonoma">Light Sonoma</option>
                         <option value="Caramelo">Caramelo</option>
                         <option value="Blanco">Blanco</option>
+                        <option value="Natural">N/A</option>
                         
                     </select>
                    
@@ -329,7 +330,7 @@ function ClientePage() {
                                 <td className="px-4 py-2">
                     <button 
                         onClick={() => removeFromCart(idx)}
-                        className="bg-red-100 text-red-600 hover:bg-red-600 hover:text-white p-2 rounded-lg transition-all duration-300 flex items-center justify-center mx-auto"
+                        className=" text-red-600 hover:text-white p-2 rounded-lg transition-all duration-300 flex items-center justify-center mx-auto "
                         title="Eliminar producto"
                     >
                         
